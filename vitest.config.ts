8f8c986c6053a126402d41a18596bf1a043c6149
Vishwakarma-kambar <vishwavv4768@gmail.com>
2025-09-20 23:37:22 +0530

    add deploy code

--- vitest.config.ts
+++ vitest.config.ts
@@ -1,17 +1,18 @@
-import { defineConfig } from 'vitest/config';
-import react from '@vitejs/plugin-react-swc';
-import path from 'path';
+import { defineConfig } from "vitest/config";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 export default defineConfig({
   plugins: [react()],
+  base: "/quick-find",
   test: {
-    environment: 'jsdom',
-    setupFiles: ['./src/test-setup.ts'],
+    environment: "jsdom",
+    setupFiles: ["./src/test-setup.ts"],
     globals: true,
   },
   resolve: {
     alias: {
-      '@': path.resolve(__dirname, './src'),
+      "@": path.resolve(__dirname, "./src"),
     },
   },
 });
